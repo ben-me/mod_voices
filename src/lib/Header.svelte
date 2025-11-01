319d22c998879c71f7dff19de03b44195cfa84b5
Ben M <109357901+ben-me@users.noreply.github.com>
2025-11-01 20:05:23 +0100

    feat: Add hero and header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,35 @@
+<script lang='ts'>
+  import { resolve } from '$app/paths'
+</script>
+
+<header class='absolute w-full z-1 flex font-semibold items-center justify-between p-2 md:px-8'>
+  <h1 class='text-2xl font-bold'>
+    <a href={resolve('/')}><span class='text-primary'>Mod</span>Voices</a>
+  </h1>
+  <nav>
+    <ul class='flex items-center gap-4'>
+      <li>
+        <a href={resolve('/projects')} class='inline-block p-2'>Mod-Projekte</a>
+      </li>
+      <li>
+        <a href={resolve('/voices')} class='inline-block p-2'>Synchronsprecher</a>
+      </li>
+      <li>
+        <a
+          href={resolve('/signup')}
+          class='inline-block rounded-xs text-primary border-2 border-primary px-4 py-0.5'
+        >
+          Registrieren
+        </a>
+      </li>
+      <li>
+        <a
+          href={resolve('/login')}
+          class='inline-block rounded-xs border-3 border-primary bg-primary text-black px-4 py-0.5 font-semibold'
+        >
+          Login
+        </a>
+      </li>
+    </ul>
+  </nav>
+</header>
---
+++ src/routes/(home)/+page.svelte
@@ -0,0 +1,35 @@
+<script lang='ts'>
+  import { resolve } from '$app/paths'
+  import hero from '$lib/assets/hero.jpg'
+</script>
+
+<div class='relative'>
+  <img
+    src={hero}
+    alt='Bild aus Gothic 1 und 2 mit Sicht auf das alte Lacker und die Stadt Khorinis'
+    class='relative opacity-55 max-h-dvh h-dvh object-cover 2xl:object-fill' width='3453' height='1440'
+  />
+  <div class='absolute inset-0 h-max m-auto grid gap-4 w-3/4 object-center place-items-center'>
+    <h2 class='text-6xl leading-22 font-meta text-shadow-lg text-shadow-black text-center my-[0_1rem]'>
+      Erwecke Charaktere <br>
+      mit echten Stimmen <br>
+      zum Leben.
+    </h2>
+    <div class='flex gap-4'>
+      <a
+        href={resolve('/projects')}
+        class='px-4 py-2 bg-primary text-black font-semibold rounded border border-primary hover:bg-primary/80
+          transition duration-200'
+      >
+        Projekt finden
+      </a>
+      <a
+        href={resolve('/login')}
+        class='px-4 py-2 text-primary font-semibold rounded border border-primary hover:bg-primary/10
+          transition duration-200'
+      >
+        Stimme anbieten
+      </a>
+    </div>
+  </div>
+</div>
