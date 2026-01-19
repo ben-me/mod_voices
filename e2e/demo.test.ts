f2a711d5c0c91fadc06a6e5a349e7640a614ccf6
Ben M <109357901+ben-me@users.noreply.github.com>
2026-01-19 17:03:06 +0100

    feat: Change eslint config, create github workflow, test with playwright

--- e2e/demo.test.ts
+++ e2e/demo.test.ts
@@ -1,4 +1,17 @@
-import { expect, test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
+
+test.describe("homepage", () => {
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    await page.goto("https://your-site.com/"); // 3
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
 
 test("home page has expected h1", async ({ page }) => {
   await page.goto("/");
