9937f6d4e8035c30f10a24fc11abbd6b8c90cbd3
Ben M <109357901+ben-me@users.noreply.github.com>
2026-01-20 16:54:02 +0100

    feat: Add two factor and generate new auth schema

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -3,10 +3,14 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { sveltekitCookies } from "better-auth/svelte-kit";
 import { db } from "./db";
+import { admin, twoFactor } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
-  plugins: [sveltekitCookies(getRequestEvent)],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [sveltekitCookies(getRequestEvent), admin(), twoFactor()],
 });
