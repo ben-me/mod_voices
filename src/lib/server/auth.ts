3391829c86a0b0bd1e92f98a95ab382380051daa
Ben M <109357901+ben-me@users.noreply.github.com>
2026-01-21 12:32:33 +0100

    fix: Install postgres and

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -3,6 +3,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { sveltekitCookies } from "better-auth/svelte-kit";
 import { db } from "./db";
+import { env } from "$env/dynamic/private";
 import { admin, twoFactor } from "better-auth/plugins";
 
 export const auth = betterAuth({
@@ -13,4 +14,5 @@ export const auth = betterAuth({
     enabled: true,
   },
   plugins: [sveltekitCookies(getRequestEvent), admin(), twoFactor()],
+  secret: env.BETTER_AUTH_SECRET,
 });
