7be0c52358313d6962d256953c51b6cbe23de988
Ben M <109357901+ben-me@users.noreply.github.com>
2026-01-23 14:02:38 +0100

    feat: Update gitignore and install postcss global data plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,15 @@
 import postcssPresetEnv from "postcss-preset-env";
 import atImport from "postcss-import";
+import postcssCustomMedia from "postcss-custom-media";
+import postcssGlobalData from "@csstools/postcss-global-data";
 
-export default config = {
+export default {
   plugins: [
     atImport(),
+    postcssGlobalData({
+      files: ["./src/styles/variables.css"],
+    }),
+    postcssCustomMedia(),
     postcssPresetEnv({
       stage: 3,
       features: {
