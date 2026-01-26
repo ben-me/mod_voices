609aee44a9e273e2eeb32a2fac36f1c79b177f9a
Ben M <109357901+ben-me@users.noreply.github.com>
2026-01-26 16:01:32 +0100

    fix: Only use port in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   webServer: {
     command: "npm run build && npm run preview",
-    url: "http://localhost",
     port: 4173,
   },
   testDir: "e2e",
