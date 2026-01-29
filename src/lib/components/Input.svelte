d146173e86255bfbbd007b0b2ed6b3e6e19c4685
Ben M <109357901+ben-me@users.noreply.github.com>
2026-01-29 16:13:11 +0100

    feat: Improve input with remoteFormIssue rendering

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -1,34 +1,44 @@
 <script lang="ts" generics="T">
+  import type { RemoteFormIssue } from "@sveltejs/kit";
   import { useDebounce } from "runed";
   import type { HTMLInputAttributes } from "svelte/elements";
   import * as v from "valibot";
 
   type Props = {
     validateOn: v.GenericSchema<T>;
+    issues?: RemoteFormIssue[] | undefined;
   } & HTMLInputAttributes;
 
-  const { validateOn, ...rest }: Props = $props();
+  const { validateOn, issues, ...rest }: Props = $props();
   let value = $state("");
-  let issues: null | v.BaseIssue<unknown>[] = $state(null);
+  let inputIssues: RemoteFormIssue[] | v.BaseIssue<unknown>[] | undefined =
+    $derived(issues);
 
   const validate_input = useDebounce(() => {
     if (value === "") {
+      inputIssues = undefined;
       return;
     }
     try {
       v.parse(validateOn, value);
-      issues = null;
+      inputIssues = undefined;
     } catch (error) {
       const validation_error = error as v.ValiError<typeof validateOn>;
-      issues = validation_error.issues;
+      inputIssues = validation_error.issues;
     }
-  }, 250);
+  }, 350);
 </script>
 
-<input {...rest} oninput={validate_input} bind:value />
-{#if issues}
+<input
+  {...rest}
+  oninput={validate_input}
+  data-input-state={inputIssues && inputIssues.length > 0 ? "error" : null}
+  aria-invalid={inputIssues && inputIssues.length > 0}
+  bind:value
+/>
+{#if inputIssues}
   <ul role="list">
-    {#each issues as issue (issue.message)}
+    {#each inputIssues as issue (issue.message)}
       <li>{issue.message}</li>
     {/each}
   </ul>
@@ -36,13 +46,20 @@
 
 <style>
   input {
-    display: block;
-    width: 100%;
     margin-block-start: 0.25rem;
+    border: 2px var(--c-black-8) solid;
     background-color: var(--c-white);
-    padding: 0.25rem;
+    padding: 0.5rem;
     color: var(--c-black);
     border-radius: var(--br-1);
+
+    &[data-input-state="error"] {
+      border-color: red;
+    }
+  }
+
+  ul {
+    margin-block-start: 0.25rem;
   }
 
   li {
