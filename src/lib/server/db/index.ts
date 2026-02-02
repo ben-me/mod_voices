00ae56b685eefef7eb992e3d863b4ee6141f21f6
Ben M <109357901+ben-me@users.noreply.github.com>
2026-02-02 17:13:41 +0100

    fix: Fix relations after upgrade of drizzle

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,10 +2,11 @@ import { env } from "$env/dynamic/private";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { user } from "./schema";
 import { eq } from "drizzle-orm";
+import { relations } from "./relations";
 
 if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
-export const db = drizzle(env.DATABASE_URL);
+export const db = drizzle(env.DATABASE_URL, { relations: { ...relations } });
 
 export async function get_user(
   username: string,
