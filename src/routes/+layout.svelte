50be0c58eb58181089b8e663879572fc1897a9fc
Ben M <109357901+ben-me@users.noreply.github.com>
2026-02-02 17:14:48 +0100

    feat: Add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
   import "./style.css";
-  import favicon from "$lib/assets/favicon.svg";
   import Header from "$lib/Header.svelte";
 
   const { children } = $props();
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  <link rel="manifest" href="/site.webmanifest" />
   <meta name="color-scheme" />
 </svelte:head>
 
