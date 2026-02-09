9835e40ec4df4114742428d77d894526fde0038c
Ben M <109357901+ben-me@users.noreply.github.com>
2026-02-09 18:50:34 +0100

    feat: Add new db operation to create a project

--- src/lib/server/db/operations.ts
+++ src/lib/server/db/operations.ts
@@ -1,6 +1,8 @@
 import { getRequestEvent } from "$app/server";
+import type { ProjectInput } from "$lib/utilities/validation-schemas";
 import { db } from ".";
 import { auth } from "../auth";
+import { projectListing, voiceDescription } from "./schema";
 
 export async function get_user_from_session() {
   const event = getRequestEvent();
@@ -8,18 +10,33 @@ export async function get_user_from_session() {
     headers: event.request.headers,
   });
 
-  return {
-    user: session?.user.id,
-  };
+  return session?.user.id;
 }
 
-export async function get_user_by_name(
-  username: string,
-): Promise<{ name: string } | undefined> {
+export async function get_user_by_name(username: string) {
   const user = await db.query.user.findFirst({
     where: {
       name: username,
     },
   });
   return user;
+}
+
+export async function create_new_project({
+  title,
+  voiceDescriptions,
+  description,
+  userId,
+}: ProjectInput & { userId: string }) {
+  const project_response = await db
+    .insert(projectListing)
+    .values({ title, description, userId, active: true })
+    .returning({ projectId: projectListing.id });
+
+  voiceDescriptions.forEach(async (voice) => {
+    await db.insert(voiceDescription).values({
+      projectId: project_response[0].projectId,
+      ...voice,
+    });
+  });
 }
